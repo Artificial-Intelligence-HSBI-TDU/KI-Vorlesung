3f50ae9c0191894a5a0fcd7f26257c0d37a05096
Carsten Gips <cagix@fh-bielefeld.de>
2021-09-28 19:47:23 +0200

    Add support for dot (#66)
    * Makefile: add support for dot files
    
    * add fonts to docker image for use with dot/graphviz

--- .github/actions/alpine-pandoc-hugo/install-packages.sh
+++ .github/actions/alpine-pandoc-hugo/install-packages.sh
@@ -10,7 +10,7 @@ tlmgr install beamertheme-metropolis pgfopts tcolorbox environ || exit 1
 tlmgr install standalone filemod currfile || exit 1
 
 ## Install make and graphviz (dot)
-apk --no-cache add make bash graphviz ghostscript || exit 1
+apk --no-cache add make bash graphviz ghostscript font-noto || exit 1
 
 ## Install current Hugo
 wget https://github.com/gohugoio/hugo/releases/download/v0.87.0/hugo_0.87.0_Linux-64bit.tar.gz && tar -zxf hugo_*.tar.gz -C /usr/bin/ && rm hugo_*.tar.gz  || exit 1
